a57a2a5e56a430aaea1eefc242986d0023652f38
Oscar Jané <oscar.gjane@gmail.com>
2023-03-17 14:00:38 +0100

    refactor: refactored api route code to call sengrid api endpoint

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,37 +1,21 @@
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-export default function handler(req, res) {
+export default async function Handler(req, res) {
   const body = JSON.parse(req.body);
 
   const message = `
-        Name: ${body.fullName}rn
-        Email: ${body.emailAddress}rn
-        Subject: ${body.subject}rn
-        Message: ${body.message}rn
-    `;
+    You've got a new mail from ${body.fullName}, their email is: ✉️${body.emailAddress}\r\n
+    Message: ${body.message}
+  `;
 
-  (async () => {
-    try {
-      await sgMail
-        .send({
-          to: 'hello@oscar-jane.com',
-          from: 'hello@oscar-jane.com',
-          subject: 'New Message!',
-          text: message,
-          html: message.replace(/rn/g, '<br>'),
-        })
-        .then(() => {
-          res
-            .status(200)
-            .json({ status: 'Message has been sent successfully :)' });
-        });
-    } catch (error) {
-      console.log(error);
+  await sgMail.send({
+    to: 'hello@oscar-jane.com',
+    from: 'hello@oscar-jane.com',
+    subject: `[Personal website]: ${body.subjectMessage}`,
+    text: message,
+    html: message.replace(/\r\n/g, '<br>'),
+  });
 
-      if (error.response) {
-        console.error(error.response.body);
-      }
-    }
-  })();
+  res.status(200).json({ status: 'Ok' });
 }
