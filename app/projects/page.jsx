14a98f151cb4e9521ce1f773faf7684bf7701efb
Oscar Jané <oscar.gjane@gmail.com>
2023-04-04 15:35:13 +0200

    styles: added a mobile-friendly projects page

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -28,46 +28,47 @@ export default async function ProjectsPage() {
         I am constantly seeking to acquire new skills and develop myself both
         personally and professionally. Here are some of my personal projects.
       </p>
-      <div className="w-screen mt-12 ml-[-50%] flex gap-5 justify-center">
+      <ul className="w-full lg:w-screen flex flex-col lg:flex-row lg:justify-center mt-12 lg:ml-[-50%] gap-5 ">
         {Object.entries(projectItem).map(
           ([id, { name, overview, tags, url }]) => {
             return (
-              <a
+              <li
                 key={id}
-                href={url}
-                target="_blank"
                 className={clsx(
-                  'flex flex-col w-1/3 p-4 border border-neutral-200 rounded-lg hover:bg-neutral-50 transition-all',
+                  'w-full flex flex-col lg:w-1/3 p-4 border border-neutral-200 rounded-lg hover:bg-neutral-50 transition-all',
                   { 'cursor-not-allowed': id === 'mountain' }
                 )}
-                rel="noreferrer"
               >
-                <h2 className="font-bold flex items-center">
-                  {name}
-                  {id === 'mountain' ? (
-                    <span className="ml-3 py-1 px-2 rounded-lg text-xs  text-white bg-neutral-900">
-                      coming soon
-                    </span>
-                  ) : null}
-                </h2>
-                <p className="my-5 text-base text-neutral-700">{overview}</p>
-                <ul className="flex space-x-2">
-                  {tags.map((tag) => {
-                    return (
-                      <li
-                        key={tag}
-                        className="py-1 px-2 rounded-lg bg-neutral-100"
-                      >
-                        <span className="text-sm text-neutral-700">{tag}</span>
-                      </li>
-                    );
-                  })}
-                </ul>
-              </a>
+                <a href={url} target="_blank" rel="noreferrer">
+                  <h2 className="font-bold flex items-center">
+                    {name}
+                    {id === 'mountain' ? (
+                      <span className="ml-3 py-1 px-2 rounded-lg text-xs  text-white bg-neutral-900">
+                        coming soon
+                      </span>
+                    ) : null}
+                  </h2>
+                  <p className="my-5 text-base text-neutral-700">{overview}</p>
+                  <ul className="flex flex-wrap gap-2">
+                    {tags.map((tag) => {
+                      return (
+                        <li
+                          key={tag}
+                          className="self-center py-1 px-2 rounded-lg bg-neutral-100"
+                        >
+                          <span className="text-sm text-neutral-700">
+                            {tag}
+                          </span>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </a>
+              </li>
             );
           }
         )}
-      </div>
+      </ul>
     </section>
   );
 }
